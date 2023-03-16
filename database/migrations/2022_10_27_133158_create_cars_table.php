bca814e35cd27a4cf01ecb2ca71897922e7207bb
Abderrahim0419 <jabane.abderrahim@gmail.com>
2023-03-16 11:27:08 +0100

    add champs slug in migration

--- database/migrations/2022_10_27_133158_create_cars_table.php
+++ database/migrations/2022_10_27_133158_create_cars_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
+            $table->string('slug')->unique();
             $table->string('image');
             $table->timestamps();
         });
