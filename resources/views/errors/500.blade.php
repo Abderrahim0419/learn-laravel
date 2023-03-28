a4fa702bf0608b6e5584a97e5cc29f2529bed906
Abderrahim0419 <abderahim.jabane@gmail.com>
2023-03-28 15:09:32 +0000

    change theme of page error

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,100 +1,99 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>market-wp.com</title>
+  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <style>
-        * {
-  font-family: Google sans, Arial;
+        #oopss {
+  background: linear-gradient(-45deg, #064C92, #064C89);
+  position: fixed;
+  left: 0px;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  line-height: 1.5em;
+  z-index: 9999;
 }
-
-html, body {
-  margin: 0;
-  padding: 0;
-}
-
-.flex-container {
+#oopss #error-text {
+  font-size: 40px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  height: 100vh;
-  color: white;
-  animation: colorSlide 15s cubic-bezier(0.075, 0.82, 0.165, 1) infinite;
-
-  .text-center {
-    text-align: center;
-    h1,
-    h3 {
-      margin: 10px;
-      cursor: default;
-      
-      .fade-in {
-        animation: fadeIn 2s ease infinite;
-      }
-    }
-
-    h1 {
-      font-size: 8em;
-      transition: font-size 200ms ease-in-out;
-      border-bottom: 1px dashed white;
-
-      span#digit1 { animation-delay: 200ms; }
-      span#digit2 { animation-delay: 300ms; }
-      span#digit3 { animation-delay: 400ms; }
-    }
-    
-    button {
-      border: 1px solid white;
-      background: transparent;
-      outline: none;
-      padding: 10px 20px;
-      font-size: 1.1rem;
-      font-weight: bold;
-      color: white;
-      text-transform: uppercase;
-      transition: background-color 200ms ease-in;
-      margin: 20px 0;
-      
-      &:hover {
-        background-color: white;
-        color: #555;
-        cursor: pointer;
-      }
-    }
-  }
+  font-family: 'Shabnam', Tahoma, sans-serif;
+  color: #fff;
+  direction: rtl;
+}
+#oopss #error-text img {
+  margin: 85px auto 20px;
+  height: 342px;
+}
+#oopss #error-text span {
+  position: relative;
+  font-size: 3.3em;
+  font-weight: 900;
+  margin-bottom: 50px;
+}
+#oopss #error-text p.p-a {
+  font-size: 19px;
+  margin: 30px 0 15px 0;
+}
+#oopss #error-text p.p-b {
+  font-size: 15px;
+}
+#oopss #error-text .back {
+  background: #fff;
+  color: #000;
+  font-size: 30px;
+  text-decoration: none;
+  margin: 2em auto 0;
+  padding: .7em 2em;
+  border-radius: 500px;
+  box-shadow: 0 20px 70px 4px rgba(0, 0, 0, 0.1), inset 7px 33px 0 0px #fff300;
+  font-weight: 900;
+  transition: all 300ms ease;
+}
+#oopss #error-text .back:hover {
+  -webkit-transform: translateY(-13px);
+          transform: translateY(-13px);
+  box-shadow: 0 35px 90px 4px rgba(0, 0, 0, 0.3), inset 0px 0 0 3px #000;
 }
 
-@keyframes colorSlide {
-  0% { background-color: #152a68; }
-  25% { background-color: royalblue; }
-  50% { background-color: seagreen; }
-  75% { background-color: tomato; }
-  100% { background-color: #152a68; }
+@font-face {
+  font-family: Shabnam;
+  src: url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam-Bold.eot");
+  src: url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam-Bold.eot?#iefix") format("embedded-opentype"), url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam-Bold.woff") format("woff"), url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam-Bold.woff2") format("woff2"), url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam-Bold.ttf") format("truetype");
+  font-weight: bold;
+}
+@font-face {
+  font-family: Shabnam;
+  src: url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam.eot");
+  src: url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam.eot?#iefix") format("embedded-opentype"), url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam.woff") format("woff"), url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam.woff2") format("woff2"), url("https://cdn.rawgit.com/ahmedhosna95/upload/ba6564f8/fonts/Shabnam/Shabnam.ttf") format("truetype");
+  font-weight: normal;
 }
 
-@keyframes fadeIn {
-  from {
-    opacity: 0;
-  };
-  100% {
-    opacity: 1;
-  }
-}
     </style>
 </head>
+
 <body>
-    <div class="flex-container">
-        <div class="text-center">
-          <h1>
-            <span class="fade-in" id="digit1">4</span>
-            <span class="fade-in" id="digit2">0</span>
-            <span class="fade-in" id="digit3">4</span>
-          </h1>
-          <h3 class="fadeIn">PAGE NOT FOUND</h3>
-          <button type="button" name="button">Return To Home</button>
-        </div>
-      </div>
+
+  <div id='oopss'>
+    <div id='error-text'>
+        <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404">
+        <span>404 PAGE</span>
+        <p class="p-a">
+           . The page you were looking for could not be found</p>
+        <p class="p-b">
+            ... Back to previous page
+        </p>
+        <a href='#' class="back">... Back to previous page</a>
+    </div>
+</div>
+  
+  
+
 </body>
+
 </html>
