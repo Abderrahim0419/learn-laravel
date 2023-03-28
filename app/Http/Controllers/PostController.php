3071f6e2969ea8f5a5edd49c0860a46d3c571602
Abderrahim0419 <abderahim.jabane@gmail.com>
2023-03-28 14:52:35 +0000

    custom page error

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,7 +27,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::Paginate(3);
+        $posts = Post::Paginate(3)
         $users = User::whereNotNull('created_at')->get();
         $post = Post::all();
         foreach ($post as $key => $value) {
